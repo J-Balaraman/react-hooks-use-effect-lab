4ca13978213c75715e56aa7d4a2c8e6033e2002c
jbalaraman <james.balaraman@gmail.com>
2023-10-02 13:31:38 -0600

    I am mind boggled

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  let timer;
 
-  // add useEffect code
+  useEffect(() => {
+    // Start the timer when the component mounts
+    timer = setTimeout(() => {
+      onAnswered(false); // Trigger incorrect answer after 10 seconds
+      setTimeRemaining(10); // Reset timer to 10 seconds
+    }, 10000);
+
+    // Decrement the timer by 1 second every second
+    const intervalId = setInterval(() => {
+      setTimeRemaining(prevTime => {
+        if (prevTime <= 1) {
+          clearInterval(intervalId); // Clear the interval when the timer reaches 0 or unmounts
+          return 10; // Reset timer to 10 seconds
+        } else {
+          return prevTime - 1; // Decrement timer
+        }
+      });
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer); // Clear the timeout when the component unmounts
+      clearInterval(intervalId); // Clear the interval when the component unmounts
+    };
+  }, [onAnswered]);
 
   function handleAnswer(isCorrect) {
+    clearTimeout(timer);
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
